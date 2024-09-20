8ebb19db4c3e53309dd3180864a6ad5e4ed58d65
Emmanuel Oringe <emmanueloringe@gmail.com>
2024-09-20 11:36:01 +0300

    Added autoload on load.php

--- classes/load.php
+++ classes/load.php
@@ -1,8 +1,17 @@
 <?php
 
 function classAutoLoad($classname){
-    $directories=[]
+    $directories=["classes"];
+
+    foreach ($directories AS $dir){
+        $filename=dirname(__FILE__).DIRECTORY_SEPERATOR.$dir.DIRECTORY_SEPERATOR.$classname.".php";
+        if(file_exists($filename) AND is_readable($filename)){
+            require_once $filename;
+        }
+    }
 }
+
+spl_autpload_register('classAutoLoad');
 
 
 ?>
